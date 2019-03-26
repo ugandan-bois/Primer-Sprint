c5a6415429d6607146b5927dd6e842a822512d23
Felipe Balcero <felipe.balcero45@gmail.com>
2019-03-25 22:46:16 -0500

    Adding materialize-css and two routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const path = require('path');
-const hbs = require('hbs');
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
 
-const app = express();
-const publicsDIr = path.join(__dirname, '../public');
-const partialsDir = path.join(__dirname, '../partials');
+const app = express()
+const publicsDIr = path.join(__dirname, '../public')
+const partialsDir = path.join(__dirname, '../partials')
+const nodeModulesDir = path.join(__dirname, '../node_modules')
 
-app.use(express.static(publicsDIr));
-app.set('view engine', 'hbs');
-hbs.registerPartials(partialsDir);
+app.use(express.static(publicsDIr))
+app.set('view engine', 'hbs')
+hbs.registerPartials(partialsDir)
+
+app.use('/css/materialize', express.static(nodeModulesDir + '/materialize-css/dist/css/materialize.min.css'))
+app.use('/js/materialize', express.static(nodeModulesDir + '/materialize-css/dist/js/materialize.min.js'))
 
 app.get('/', (req, res) => {
     res.render('index')
-});
+})
+app.get('/courses', (req, res) => {
+    res.render('courses')
+})
+app.get('/register', (req, res) => {
+    res.render('register')
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
