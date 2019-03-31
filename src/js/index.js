2fdd6d67ff95ced819f950e156350b06a6dfebde
Oscar Molano <mannenutensjel@gmail.com>
2019-03-31 13:01:24 -0500

    Adding user registration functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import RegisterUser from './components/registerUser'
 import RegisterCourse from './components/registerCourse'
 
-// const registerUser = new RegisterUser()
+const registerUser = new RegisterUser()
 const registerCourse = new RegisterCourse()
