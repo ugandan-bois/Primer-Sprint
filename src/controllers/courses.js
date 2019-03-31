3ca4efe2f2437a4a1e2a2bbc373b0eb6e88141a3
Oscar Molano <mannenutensjel@gmail.com>
2019-03-31 15:33:06 -0500

    Implementing courses service with logic for fetching and updating courses

--- src/controllers/courses.js
+++ src/controllers/courses.js
@@ -1,20 +1,30 @@
-const courses = require('../../courses.json')
-const fs = require('fs')
+const coursesService = require('../services/coursesService')
 
 const getCourses = (req, res) => {
+    const courses = coursesService.getCourses()
     res.status(200).send(courses)
 }
 
 const createCourse = (req, res) => {
     const params = req.body
+    const courses = coursesService.getCourses()
+
     if (!courses.find(course => course.id == params.id)) {
-        courses.push(params)
-        fs.writeFile('courses.json', JSON.stringify(courses), (err) => {
-            if (err) throw err
-            res.status(200).send({params: params, success: true})
-        })
+        const result = coursesService.createCourse(params)
+        result ?
+            res.status(200).send({
+                params: params,
+                success: true
+            }) :
+            res.status(500).send({
+                message: 'Error inesperado agregando el curso',
+                success: false
+            })
     } else {
-        res.status(409).send({message: 'Ya existe un curso con este id', success: false})
+        res.status(409).send({
+            message: 'Ya existe un curso con este id',
+            success: false
+        })
     }
 }
 
