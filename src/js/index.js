5f8e519ea87f165c5372d04b2bba86b22f16026d
Luis Bermudez <luisbermudeztorres@gmail.com>
2019-03-31 01:15:43 -0500

    list courses as interested

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,9 @@ import RegisterCourse from './components/registerCourse'
 
 // const registerUser = new RegisterUser()
 const registerCourse = new RegisterCourse()
+
+var elem = document.querySelector('.collapsible');
+var instance = new M.Collapsible(elem, {
+  // inDuration: 1000,
+  // outDuration: 1000
+});
